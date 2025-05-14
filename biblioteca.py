26a0f0ed8981f81b50b2cd064aff794b3ea951ca
Laís Serafim <laisserafim8@gmail.com>
2025-05-14 12:05:30 -0300

    subindo

--- biblioteca.py
+++ biblioteca.py
@@ -1,6 +1,6 @@
 class Pessoa():
     def __init__(self, nome, peso, idade, comendo= False, falando= False, dormindo=False):
-        self.nome= nome
+        self.nome= nome #atributo
         self.peso= peso
         self.idade= idade
         self.comendo = False
@@ -42,4 +42,98 @@ class Pessoa():
         print(f"{self.nome} parou de falar")
         self.falando = False
 
+class Animal():
+        def __init__(self,nome, cor):
+            self.nome=nome
+            self.cor=cor
 
+        def comer(self):
+            print(f"O {self.nome} foi comer...")
+
+class Gato (Animal):
+        def __init__(self,nome,cor):
+            super(). __init__(nome,cor)
+
+        def miar(self):
+            print(f"O {self.nome} foi miando...")
+
+class Cachorro (Animal):
+    def __init__(self, nome,cor):
+        super().__init__(nome, cor)
+
+    def latir(self):
+        print(f"O {self.nome} late")
+
+class Coelho(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+    def cacareja(self):
+        print(f"O {self.nome} cacareja")
+
+class Vaca(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+    def muge(self):
+        print(f"O {self.nome} muge")
+
+    def comeCapim(self):
+        print(f"O {self.nome} come capim")
+
+class Ingresso(): #classe
+    def __init__(self,valor): #método construtor
+        self.valor= valor
+
+    def imprimeValor(self): #método
+        print(f"O valor do ingresso é R$ {self.valor}. ")
+
+class Vip(Ingresso):
+    def __init__(self,valor):
+        super().__init__(valor*1.5)
+
+    def imprimeVip(self):
+        print(f"O valor do ingresso vip é {self.valor:.2f}")
+
+
+class Forma():
+    def __init__(self):
+        self.area= 0
+        self.perimetro= 0
+
+class Retangulo(Forma):
+    def __init__(self, base, altura):
+        super().__init__()
+        self.base = base
+        self.altura = altura
+
+    def calcularArea(self):
+        self.area = self.base * self.altura
+        print(f"A base é {self.area}")
+
+    def calcularPerimetro(self):
+        self.perimetro = (self.base+self.altura)*2
+        print(self.perimetro)
+
+class Triangulo (Forma):
+    def __init__(self, base, altura):
+        super().__init__()
+        self.base = base
+        self.altura = altura
+
+    def calcularArea(self):
+        self.area = (self.base*self.altura)/2
+        print(self.area)
+    def calcularPerimetro(self):
+        self.perimetro = self.base*3
+        print(self.perimetro)
+
+
+
+retangulo = Retangulo (2,2)
+retangulo.calcularArea()
+retangulo.calcularPerimetro()
+
+triangulo = Triangulo(2,4)
+triangulo.calcularArea()
+triangulo.calcularPerimetro()
--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-from biblioteca import Pessoa
+from biblioteca import Pessoa, Gato, Cachorro, Vaca, Coelho, Ingresso, Vip
 
-aluno01= Pessoa("Igor", 75, 29)
+"""aluno01= Pessoa("Igor", 75, 29)
 
 aluno01.comer("cachorro quente")
-aluno01.falar()
+aluno01.falar()"""
 
+"""gato1= Gato("lindinho","preto")
+print(gato1.cor)
+print(gato1.nome)"""""
 
+"""gato2= Gato("lindinho","preto")
+print(gato1.cor)
+print(gato1.nome)"""
+
+"""cachorro1 = Cachorro ("mimosa", "malhada")
+print(cachorro1.nome, cachorro1.cor)
+cachorro1.latir()"""""
+
+"""""vaca1 = Vaca("Doidinha", "laranja")
+print(vaca1.nome, vaca1.cor)
+vaca1.muge()
+vaca1.comeCapim()"""
+
+"""coelho1 = Coelho("Maluco", "Azul Marinho")
+print(coelho1.nome, coelho1.cor)
+coelho1.cacareja()"""
+
+ingresso1 = Ingresso(20)
+ingresso1.imprimeValor()
+
+v = Vip(20)
+v.imprimeVip()
 
